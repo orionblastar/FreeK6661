aab2f959a8cb6398fcb753c1045dec656678c040
procrasti <procrasti@k5-stats.org>
2016-05-24 11:35:20 +0800

    I'm not exactly sure what went wrong here.
    Is it just the indentation?
    
    Not hidden characters or something funky? Sometimes happens to me, I swear.
    
    Anyway, probably just the indentation.

--- freek666/urls.py
+++ freek666/urls.py
@@ -4,6 +4,6 @@ from django.views.generic.base import RedirectView
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
-url(r'^$', RedirectView.as_view(url="/static/index.html")),
+    url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', RedirectView.as_view(url="/static/index.html")),
 ]
