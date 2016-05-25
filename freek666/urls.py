2e2719e1f40d072b49a57b7a11568a4cd15f872b
Orion Blastar <orionblastar@gmail.com>
2016-05-25 09:12:16 -0500

    Adding from K666

---
+++ freek666/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+
+from django.views.generic.base import RedirectView
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
+url(r'^$', RedirectView.as_view(url="/static/index.html")),
+]
